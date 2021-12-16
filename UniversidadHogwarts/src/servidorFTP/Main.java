980b4363afe5d8fbfbe14146fb994ad23596f8c4
Álvaro Palacios Cabrera <alvaropalacioscabrera.sanjose@alumnado.fundacionloyola.net>
2021-12-16 12:02:34 +0100

    Control FTP

---
+++ UniversidadHogwarts/src/servidorFTP/Main.java
@@ -0,0 +1,33 @@
+package servidorFTP;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		ServerSocket server;
+		Socket cliente;
+		int puerto = 6000;
+		Thread hiloServ;
+		
+		try {
+			server = new ServerSocket(puerto);
+			System.out.println("Servidor iniciado...");
+			
+			while(true) {
+				cliente = new Socket();
+				cliente = server.accept();
+				
+				hiloServ = new Thread(new HiloServidor(cliente));
+				hiloServ.start();
+			}
+		}catch (IOException e) {
+			// TODO: handle exception
+		}
+		
+	}
+
+}
