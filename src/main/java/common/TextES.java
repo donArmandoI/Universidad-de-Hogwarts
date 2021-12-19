d6543beec53d4973e47cc4bb4a908fc655adddd4
IAlonsoCalzadoSJ <80317321+IAlonsoCalzadoSJ@users.noreply.github.com>
2021-12-19 21:25:37 +0100

    19/12/2021

--- src/main/java/common/TextES.java
+++ src/main/java/common/TextES.java
@@ -5,16 +5,124 @@ package common;
  * @author Armando
  *
  */
-public class TextES {
+public final class TextES {
 
-	private String main_menu_login;
+	private static String main_menu_login;
+	
+	private static String controllerMailInboundHost;
+	private static String controllerMailInboundPassword;
+	private static String controllerMailInboundPort;
+	private static String controllerMailInboundProtocol;
+	private static String controllerMailInboundSocketFactoryFallback;
+	private static String controllerMailInboundSocketFactoryPort;
+	private static String controllerMailInboundSSLSocketFactoryClass;
+	private static String controllerMailInboundUserName;
+	
 	
 	public TextES() {
 		main_menu_login = "Iniciar Sesión";
+		
+		controllerMailInboundHost = "pop.gmail.com";
+		controllerMailInboundPassword = "*****";
+		controllerMailInboundPort = "995";
+		controllerMailInboundProtocol = "pop3";
+		controllerMailInboundSocketFactoryFallback= "false";
+		controllerMailInboundSocketFactoryPort = "995";
+		controllerMailInboundSSLSocketFactoryClass = "javax.net.ssl.SSLSocketFactory";
+		controllerMailInboundUserName = "ialonsocalzado.sanjose@alumnado.fundacionloyola.net";
+		
+		
 	}
 
-	public String getMain_menu_login() {
+
+	public static String getMain_menu_login() {
 		return main_menu_login;
 	}
+
+
+	public static String getControllerMailInboundHost() {
+		return controllerMailInboundHost;
+	}
+
+
+	public static String getControllerMailInboundPassword() {
+		return controllerMailInboundPassword;
+	}
+
+
+	public static String getControllerMailInboundPort() {
+		return controllerMailInboundPort;
+	}
+
+
+	public static String getControllerMailInboundProtocol() {
+		return controllerMailInboundProtocol;
+	}
+
+
+	public static String getControllerMailInboundSocketFactoryFallback() {
+		return controllerMailInboundSocketFactoryFallback;
+	}
+
+
+	public static String getControllerMailInboundSSLSocketFactoryClass() {
+		return controllerMailInboundSSLSocketFactoryClass;
+	}
+
+
+	public static String getControllerMailInboundUserName() {
+		return controllerMailInboundUserName;
+	}
+
+
+	public static String getControllerMailInboundSocketFactoryPort() {
+		return controllerMailInboundSocketFactoryPort;
+	}
+
+
+	public static void setControllerMailInboundSocketFactoryPort(String controllerMailInboundSocketFactoryPort) {
+		TextES.controllerMailInboundSocketFactoryPort = controllerMailInboundSocketFactoryPort;
+	}
+
+
+	public static void setMain_menu_login(String main_menu_login) {
+		TextES.main_menu_login = main_menu_login;
+	}
+
+
+	public static void setControllerMailInboundHost(String controllerMailInboundHost) {
+		TextES.controllerMailInboundHost = controllerMailInboundHost;
+	}
+
+
+	public static void setControllerMailInboundPassword(String controllerMailInboundPassword) {
+		TextES.controllerMailInboundPassword = controllerMailInboundPassword;
+	}
+
+
+	public static void setControllerMailInboundPort(String controllerMailInboundPort) {
+		TextES.controllerMailInboundPort = controllerMailInboundPort;
+	}
+
+
+	public static void setControllerMailInboundProtocol(String controllerMailInboundProtocol) {
+		TextES.controllerMailInboundProtocol = controllerMailInboundProtocol;
+	}
+
+
+	public static void setControllerMailInboundSocketFactoryFallback(String controllerMailInboundSocketFactoryFallback) {
+		TextES.controllerMailInboundSocketFactoryFallback = controllerMailInboundSocketFactoryFallback;
+	}
+
+
+	public static void setControllerMailInboundSSLSocketFactoryClass(String controllerMailInboundSSLSocketFactoryClass) {
+		TextES.controllerMailInboundSSLSocketFactoryClass = controllerMailInboundSSLSocketFactoryClass;
+	}
+
+
+	public static void setControllerMailInboundUserName(String controllerMailInboundUserName) {
+		TextES.controllerMailInboundUserName = controllerMailInboundUserName;
+	}
+
 	
 }
--- src/main/java/correo/ControllerMail.java
+++ src/main/java/correo/ControllerMail.java
@@ -1,8 +1,57 @@
 package correo;
 
+import java.util.Properties;
+
+import javax.mail.MessagingException;
+import javax.mail.NoSuchProviderException;
+import javax.mail.Session;
+import javax.mail.Store;
+
+import common.TextES;
 import correo.vistaCorreo.VistaCorreo;
 
 public class ControllerMail {
 	VistaCorreo mailView;
 	
+	public ControllerMail(VistaCorreo view) {
+		mailView = view;
+		Properties props = prepareConnectionProperties();
+		readMails(props);
+	}
+
+	private void readMails(Properties props) {
+		// TODO Auto-generated method stub
+		Session session = Session.getDefaultInstance(props);
+		try {
+			Store store = session.getStore();
+			store.connect(TextES.getControllerMailInboundUserName(), TextES.getControllerMailInboundPassword());
+		} catch (NoSuchProviderException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private Properties prepareConnectionProperties() {
+		// TODO Auto-generated method stub
+		Properties props = new Properties();
+		props.setProperty("mail.pop3.host", TextES.getControllerMailInboundHost());
+		props.setProperty("mail.pop3.port", TextES.getControllerMailInboundPort());
+		props.setProperty("mail.pop3.socketFactory.class", TextES.getControllerMailInboundSSLSocketFactoryClass());
+		props.setProperty("mail.pop3.socketFactory.fallback", TextES.getControllerMailInboundSocketFactoryFallback());
+		props.setProperty("mail.pop3.socketFactory.port", TextES.getControllerMailInboundSocketFactoryPort());
+		return props;
+	}
+	// Acceder al correo y leer los mensajes de la carpeta de recibidos
+	// Agregar cada correo como un item
+	// Agregarle el evento a cada encabezado
+	// Cuando se pulse en un encabezado, se extrae el número de mensaje, se busca
+		// el mensaje con ese número y se lanza la vista del visor con ese mensaje
+	// Listener botón crear
+	// Listener botón nuevo -> enviar
+	// Hilo de sincronía
+	// Tubería para la sincronía
+		
 }
