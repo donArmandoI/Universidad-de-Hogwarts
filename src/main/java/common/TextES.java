aa898c9c0e059e7eadba59cf696300c84f028316
IAlonsoCalzadoSJ <80317321+IAlonsoCalzadoSJ@users.noreply.github.com>
2021-12-19 12:40:48 +0100

    actualizado fichero de textos

--- src/main/java/common/TextES.java
+++ src/main/java/common/TextES.java
@@ -7,14 +7,96 @@ package common;
  */
 public class TextES {
 
-	private String main_menu_login;
+	private String signinStringUserName;
+	private String signinStringName;
+	private String signinStringSurname;
+	private String signinStringTeacher;
+	private String signinStringEmail;
+	private String signinStringPassword;
+	private String signinStringLogin;
+	private String signinStringSignin;
+	private String signinStringInfo;
+	private String loginStringInfo;
 	
 	public TextES() {
-		main_menu_login = "Iniciar Sesión";
+		signinStringLogin = "Iniciar Sesión";
+		signinStringSignin = "Registrarse";
+		signinStringUserName = "Usuario";
+		signinStringPassword = "Contraseña";
+		signinStringEmail = "Correo Electrónico";
+		signinStringTeacher = "¿Eres Profesor?";
+		signinStringSurname = "Apellidos";
+		signinStringName = "Nombre";
 	}
 
-	public String getMain_menu_login() {
-		return main_menu_login;
+	/**
+	 * @return the signinStringUserName
+	 */
+	public String getSigninStringUserName() {
+		return signinStringUserName;
+	}
+
+	/**
+	 * @return the signinStringName
+	 */
+	public String getSigninStringName() {
+		return signinStringName;
+	}
+
+	/**
+	 * @return the signinStringSurname
+	 */
+	public String getSigninStringSurname() {
+		return signinStringSurname;
+	}
+
+	/**
+	 * @return the signinStringTeacher
+	 */
+	public String getSigninStringTeacher() {
+		return signinStringTeacher;
+	}
+
+	/**
+	 * @return the signinStringEmail
+	 */
+	public String getSigninStringEmail() {
+		return signinStringEmail;
+	}
+
+	/**
+	 * @return the signinStringPassword
+	 */
+	public String getSigninStringPassword() {
+		return signinStringPassword;
+	}
+
+	/**
+	 * @return the signinStringLogin
+	 */
+	public String getSigninStringLogin() {
+		return signinStringLogin;
+	}
+
+	/**
+	 * @return the signinStringSignin
+	 */
+	public String getSigninStringSignin() {
+		return signinStringSignin;
+	}
+
+	/**
+	 * @return the signinStringInfo
+	 */
+	public String getSigninStringInfo() {
+		return signinStringInfo;
+	}
+
+	/**
+	 * @return the loginStringInfo
+	 */
+	public String getLoginStringInfo() {
+		return loginStringInfo;
 	}
 	
 }
